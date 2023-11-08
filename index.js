f3a50bd58959208494c8900158e057f11a782424
mihad-khadem <mihadkhadem@gmail.com>
2023-11-09 00:23:20 +0600

    Authntication with jwt

--- index.js
+++ index.js
@@ -11,7 +11,35 @@ const cookieParser = require('cookie-parser')
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+app.use(cookieParser())
+
+
+// Creating own middleware
+const logger = async(req, res, next) => {
+  console.log('called: ', req.hostname, req.originalUrl);
+  next();
+}
+const VerifyToken = async(req, res, next) => {
+  const token  = req.cookies?.token;
+  console.log('Value of token: ', token);
+  if (!token) {
+    return res.status(401).send({message:'Not authorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({message: 'Unauthorized'})
+    }
+    console.log('value in the token: ', decoded);
+    req.user = decoded;
+
+  })
+  next();
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,17 +60,18 @@ const client = new MongoClient(uri, {
       app.post('/jwt', async (req, res) => {
         const user = req.body
         console.log(user);
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30min'})
-        res.cookie('token', token, {
-          httpOnly: true,
-          secure: false,
-          sameSite: false
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '5min'
         })
-        .send({success: true})
+        res.cookie('token', token, { 
+          httpOnly: true,
+          secure: false, 
+          sameSite: false
+        }).send({success: true})  
       })
 
-      app.get('/services', async(req, res) => {
-        const cursor = await serviceCollection.find().toArray();
+      app.get('/services', logger, async(req, res) => {
+        const cursor = await serviceCollection.find().toArray(); 
         res.send(cursor)
       })
       app.get('/services/:id', async(req, res) => {
@@ -59,16 +88,21 @@ const client = new MongoClient(uri, {
     //   Send User booking info to database
     const bookingCollection = client.db('car-dr-db').collection('bookings')
     // Load user booking data
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', logger, VerifyToken, async (req, res) => {
         // console.log(req.query.email);
+        // console.log('token',req.cookies.token);
+        console.log('Validated token',req.user);
+        if(req.query?.email !== req.user?.email) {
+          return res.status(403).send({message: 'Forbidden access'})
+        }
         let query = {}
         if(req.query?.email){
             query = {customerEmail: req.query.email}
         }
-        const result = await bookingCollection.find(query).toArray() 
+        const result = await bookingCollection.find(query).toArray()  
         res.send(result);
-    })
-    app.post('/bookings', async(req, res) => {
+    }) 
+    app.post('/bookings', logger, async(req, res) => {
         const booking = req.body;
         // console.log(booking);
         const result = await bookingCollection.insertOne(booking);
@@ -77,12 +111,12 @@ const client = new MongoClient(uri, {
     app.delete('/bookings/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
-      const result = await bookingCollection.deleteOne(query);
+      const result = await bookingCollection.deleteOne(query); 
       res.send(result);
     })
     app.patch('/bookings/:id', async(req, res) => {
       const updatedBookings = req.body
-      const id = req.params.id;
+      const id = req.params.id; 
       const filter = {_id: new ObjectId(id)};
       const updatedDoc = {
         $set: {
