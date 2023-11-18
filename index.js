4dfa2672273b34a5d0ba122ec00552826d0a2c76
mihad-khadem <mihadkhadem@gmail.com>
2023-11-18 19:46:35 +0600

    Sever deployed to vercel

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cookieParser = require('cookie-parser')
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://car-doctor-react-78cbe.web.app', 'https://car-doctor-react-78cbe.firebaseapp.com'],
   credentials: true
 }));
 app.use(cookieParser())
@@ -20,12 +20,12 @@ app.use(cookieParser())
 
 // Creating own middleware
 const logger = async(req, res, next) => {
-  console.log('called: ', req.hostname, req.originalUrl);
+  console.log('called: ', req.hostname, req.method);
   next();
 }
 const VerifyToken = async(req, res, next) => {
   const token  = req.cookies?.token;
-  console.log('Value of token: ', token);
+  // console.log('Value of token: ', token);
   if (!token) {
     return res.status(401).send({message:'Not authorized'})
   }
@@ -34,7 +34,7 @@ const VerifyToken = async(req, res, next) => {
       console.log(err);
       return res.status(401).send({message: 'Unauthorized'})
     }
-    console.log('value in the token: ', decoded);
+    // console.log('value in the token: ', decoded);
     req.user = decoded;
 
   })
@@ -52,22 +52,28 @@ const client = new MongoClient(uri, {
   async function run() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+      // await client.connect(); 
         
       const serviceCollection = client.db('car-dr-db').collection('servicesCart')
 
-      // Auth related api
+      // Auth related api(How to generate a token using JWT ? )
       app.post('/jwt', async (req, res) => {
         const user = req.body
-        console.log(user);
+        // console.log(user);
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '5min'
+          expiresIn: '1h'
         })
         res.cookie('token', token, { 
-          httpOnly: true,
-          secure: false, 
-          sameSite: false
+          httpOnly: true, 
+          secure: true, 
+          sameSite: 'none'
         }).send({success: true})  
+      })
+      // User log out 
+      app.post('/logout', async(req, res) => {
+        const user  = req?.body; 
+        console.log('logged out', user);
+        res.clearCookie('token', {maxAge: 0}).send({success: true});
       })
 
       app.get('/services', logger, async(req, res) => {
@@ -82,22 +88,22 @@ const client = new MongoClient(uri, {
         }
         const result = await serviceCollection.findOne(query, options)
         res.send(result)
-        console.log(result);
+        // console.log(result);
       })
 
     //   Send User booking info to database
     const bookingCollection = client.db('car-dr-db').collection('bookings')
-    // Load user booking data
+    //! Load user booking data
     app.get('/bookings', logger, VerifyToken, async (req, res) => {
         // console.log(req.query.email);
-        // console.log('token',req.cookies.token);
+        // console.log('Cookies',req.cookies);
         console.log('Validated token',req.user);
         if(req.query?.email !== req.user?.email) {
           return res.status(403).send({message: 'Forbidden access'})
         }
         let query = {}
         if(req.query?.email){
-            query = {customerEmail: req.query.email}
+            query = {customerEmail: req.query?.email}
         }
         const result = await bookingCollection.find(query).toArray()  
         res.send(result);
@@ -108,7 +114,7 @@ const client = new MongoClient(uri, {
         const result = await bookingCollection.insertOne(booking);
         res.send(result);
     })
-    app.delete('/bookings/:id', async(req, res) => {
+    app.delete('/bookings/:id', async(req, res) => {   
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await bookingCollection.deleteOne(query); 
@@ -139,10 +145,10 @@ const client = new MongoClient(uri, {
     //   await client.close();
     }
   }
-  run().catch(console.dir);
+  run().catch(console.dir);   
 
 
-app.get('/' , (req, res) => {
+app.get('/' , (req, res) => { 
     res.send('Server Running')
 })
 app.listen(port, () => {
